41bb19432e3c3a9a08bf396aaec7355886a3585a
Khalishazahwa15 <khalishazahwa360@gmail.com>
2025-11-16 23:46:00 +0700

    Correct PHP syntax and delete query for id_sewa
    Fix syntax errors and improve SQL query execution.

--- hapus.php
+++ hapus.php
@@ -1,5 +1,14 @@
 <?php
-  include 'koneksi.php'
-    $id_sewa = $_POST['id_sewa
+  include 'koneksi.php';
+
+  $id_sewa = $_POST['id_sewa'];
+  $sql_hapus_peminjaman : "delete from penyewaan where id_sewa = '$id_sewa' ";
+  mysqli_query($koneksi, $sql_hapus_peminjaman);
+
+?>
+
+
+    
+    
 
 
