79e58b19c51ac5903c1783bdd76be15321bd7239
Khalishazahwa15 <khalishazahwa360@gmail.com>
2025-11-18 22:47:34 +0700

    Update header template with new title and comments

---
+++ templates/header.php
@@ -0,0 +1,28 @@
+<?php
+// template untuk header 
+// ketika mau menghubungan atau menampilkan header panggil dengan 'templates/footer.php'
+?>
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Sistem Rental Mobil</title> 
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <!-- untuk menampilkan cssnya -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet"> <!--terhubung ke css yg buat tampilan lebih menarik-->
+</head>
+<body class="bg-light"> <!--warna background-->
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary shadow-sm">
+  <div class="container">
+    <a class="navbar-brand fw-bold" href="index.php">Rental Mobil Gen Z</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+  </div>
+</nav>
+
+<div class="container py-4">
