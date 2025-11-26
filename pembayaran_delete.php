9769384f82d5961927ea3b8ee74b851ec0a5b1c3
SivaSinaga <sivasinaga1105@gmail.com>
2025-11-26 09:01:27 +0700

    Delete pembayaran_delete.php

--- pembayaran_delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (!isset($_GET['id'])) {
-    header('Location: index.php?page=pembayaran');
-    exit;
-}
-$id = (int)$_GET['id'];
-mysqli_query($conn, "DELETE FROM tb_pembayaran WHERE id_bayar=$id");
-header('Location: index.php?page=pembayaran');
-exit;
